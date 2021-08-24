9dd3eba5c4c608c5047d382ec9271ca9754f3f6a
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-08-25 00:43:21 +0530

    Update backups.py

--- DewmiBot/modules/backups.py
+++ DewmiBot/modules/backups.py
@@ -372,16 +372,19 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backups ♿️"
+__mod_name__ = "Backup🗳"
 
 __help__ = """
-@szrosebot🇱🇰
+
 *Only for group owner:*
 
  ❍ `/import`: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
  Note that files / photos cannot be imported due to telegram restrictions.
 
  ❍ `/export`: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
+ 
+ 
+ ©️@TheElinaBot🇱🇰
  
 """
 
