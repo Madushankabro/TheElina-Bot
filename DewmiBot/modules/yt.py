9034dca90ef4041a98537d31b8865ee5dd9e15eb
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-08-25 21:06:17 +0530

    Update yt.py

--- DewmiBot/modules/yt.py
+++ DewmiBot/modules/yt.py
@@ -1,6 +1,8 @@
 __help__ = """
-@szrosebot🇱🇰
+
  ❍ /yt <text>: perform a youtube search
- ❍ /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+ ❍ /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server.
+ 
+   ©️@TheElinaBot🇱🇰
 """
-__mod_name__ = "Youtube💻"
+__mod_name__ = "Youtube🥌"
