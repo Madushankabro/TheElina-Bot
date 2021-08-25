aad6c500eb2e5c28a9974394a762345b16e85203
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-08-25 16:08:45 +0530

    Update _night.py

--- DewmiBot/modules/_night.py
+++ DewmiBot/modules/_night.py
@@ -1,8 +1,10 @@
 __help__ = """
-**@TheElinaBot🇱🇰**
+
  ❍ /nightmode on/off
 **Note:** Night Mode chats get Automatically closed at 12pm(IST)
 and Automatically openned at 6am(IST) To Prevent Night Spams.
+
+ ©️@TheElinaBot🇱🇰
 """
 
 __mod_name__ = "Night Mode🌖"
