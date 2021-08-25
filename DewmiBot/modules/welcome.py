88aa69114f0d33a69b5ac81b58ba2dbcd43349ed
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-08-25 21:11:39 +0530

    Update welcome.py

--- DewmiBot/modules/welcome.py
+++ DewmiBot/modules/welcome.py
@@ -1078,7 +1078,7 @@ user joined chat, user left chat.
 *Welcome markdown:* 
  ❍ `/welcomehelp`*:* view more formatting information for custom welcome/goodbye messages.
  
-
+  ©️@TheElinaBot🇱🇰
 """
 
 NEW_MEM_HANDLER = MessageHandler(Filters.status_update.new_chat_members, new_member)
@@ -1113,7 +1113,7 @@ dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
 dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 
-__mod_name__ = "Greetings🎭"
+__mod_name__ = "Greetings🎊"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
