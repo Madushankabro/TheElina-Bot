383b76ecc361e8ca879f23f5d0abf26e5ed63938
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-08-25 15:36:37 +0530

    Update __main__.py

--- DewmiBot/__main__.py
+++ DewmiBot/__main__.py
@@ -356,7 +356,16 @@ def DewmiBot_about_callback(update, context):
     query = update.callback_query
     if query.data == "aboutmanu_":
         query.message.edit_text(
-            text=f"Hay man![🇱🇰](https://telegra.ph/file/417de2001b4c28e5f696f.jpg) Click below buttons to know more about me",
+            text=f""ℹ️ I'm *𝐀𝐋𝐈𝐒𝐒𝐀*, a powerful group management bot built to help you manage your group easily.
+                 \n❍ I can restrict users.
+                 \n❍ I can greet users with customizable welcome messages and even set a group's rules.
+                 \n❍ I have an advanced anti-flood system.
+                 \n❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
+                 \n❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
+                 \n❍ I check for admins' permissions before executing any command and more stuffs
+                 \n\n_bot licensed under the GNU General Public License v3.0_
+                 \nHere is the [💾Repository](https://github.com/Madushankabro/MissAlissa).
+                 \n\nIf you have any question about amelia, let us know at @MissAlissaSupport .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
