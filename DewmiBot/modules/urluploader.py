0c235d7f1e9017342a1ec2671592939c7b02093e
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-09 18:45:12 +0530

    Ξ L I N Λ 👸

--- DewmiBot/modules/urluploader.py
+++ DewmiBot/modules/urluploader.py
@@ -28,7 +28,7 @@ async def download_coroutine(session, url, file_name, event, start, bot):
 **URL:** {}
 **File Name:** {}
 **File Size:** {}
-**© @szrosebot🇱🇰**""".format(
+**© @TheElinaBot🇱🇰**""".format(
                 url,
                 os.path.basename(file_name).replace("%20", " "),
                 humanbytes(total_length),
