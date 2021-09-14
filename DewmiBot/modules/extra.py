f9f925154e71ddca200396b65f13818e905ed70e
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-14 12:58:36 +0530

    Create extra.py

---
+++ DewmiBot/modules/extra.py
@@ -0,0 +1,11 @@
+__help__ = """
+📝CAPTION EDITOR 
+now you can edit any file caption useing ELSA BOT .
+❍/cedit <reply to file > < new caption name> :- caption editor 
+
+🔗URL SHORNER 
+Send the Long URL and get a Short URL Easily via szrosebot use this format 
+ ❍ /short <your url> :-  you can get short url useing ELSA BOT 
+ 
+"""
+__mod_name__ = "Caption editor 📝"
