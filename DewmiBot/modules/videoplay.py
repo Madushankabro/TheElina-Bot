48a0ef017158522b7f32058f6dade518749fddfb
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-14 15:16:38 +0530

    Create videoplay.py

---
+++ DewmiBot/modules/videoplay.py
@@ -0,0 +1,23 @@
+__help__ = """
+
+📚 All Command List:
+❍ /vplay (reply to video or file) - to stream video
+❍ /vstop - end the video streaming
+❍ /song (song name) - download song from YT
+❍ /vsong (video name) - download video from YT
+❍ /lyric (song name) - lyric scrapper
+🎊 FUN CMD:
+❍ /asupan - check it by yourself
+❍ /chika - check it by yourself
+❍ /wibu - check it by yourself
+❍ /truth - check it by yourself
+❍ /dare - check it by yourself
+🔰 EXTRA CMD:
+❍ /alive - check bot alive status
+❍ /ping - check bot ping status
+❍ /uptime - check bot uptime status
+❍ /sysinfo - check bot system information
+
+Powerd by @TheElinaBot🇱🇰
+"""
+__mod_name__ = "video play "
