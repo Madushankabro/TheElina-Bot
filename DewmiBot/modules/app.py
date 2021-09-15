df7cc6dd240d02423ba42f65e6e86a39648da875
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-15 19:45:04 +0530

    Update app.py

--- DewmiBot/modules/app.py
+++ DewmiBot/modules/app.py
@@ -11,7 +11,7 @@ from urllib.parse import urlencode
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image
-from search_engine_parser import GoogleSearch
+from DewmiBot import GoogleSearch
 
 import bs4
 import html2text
