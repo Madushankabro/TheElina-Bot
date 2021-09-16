90f6da244fa2e926583ab3f9f6abd3fdc55d8add
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-16 19:06:33 +0530

    Ξ L I N Λ 👸

--- DewmiBot/modules/welcome.py
+++ DewmiBot/modules/welcome.py
@@ -1113,7 +1113,7 @@ dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
 dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 
-__mod_name__ = "GREETINGS🎊"
+__mod_name__ = "ARRIVALS🙏"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
