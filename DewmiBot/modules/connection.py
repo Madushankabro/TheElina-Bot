30112179300ad23f2ca65981d56fa56d5a2e195c
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-17 23:07:28 +0530

    Update connection.py

--- DewmiBot/modules/connection.py
+++ DewmiBot/modules/connection.py
@@ -387,13 +387,14 @@ def connect_button(update, context):
         connect_chat(update, context)
 
 
-__mod_name__ = "Connection🌎"
+__mod_name__ = "CONNECT🏮"
 
 __help__ = """
 
-Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
-This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
+ ✪ Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
+ ✪ This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
 
+*Commands*
  ❍ /connect*:* Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
  ❍ /connection*:* List connected chats
  ❍ /disconnect*:* Disconnect from a chat
