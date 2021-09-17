7f05d4529c2e497d88ad956eed92500688218266
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-17 23:08:23 +0530

    updated#v1.1

--- DewmiBot/modules/connection.py
+++ DewmiBot/modules/connection.py
@@ -394,13 +394,13 @@ __help__ = """
  ✪ Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
  ✪ This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
 
-*Commands*
+*COMMANDS*
  ❍ /connect*:* Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
  ❍ /connection*:* List connected chats
  ❍ /disconnect*:* Disconnect from a chat
  ❍ /helpconnect*:* List available commands that can be used remotely
 
-*Admin only:*
+*ADMIN ONLY:*
  ❍ /allowconnect <yes/no>*:* allow a user to connect to a chat
  
  ©️@TheElinaBot🇱🇰
