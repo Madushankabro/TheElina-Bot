00e7998e925aef7b5b82d9e029ec0fa8584f1a8b
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-18 20:30:39 +0530

    Update about.py

--- DewmiBot/modules/about.py
+++ DewmiBot/modules/about.py
@@ -1,12 +1,18 @@
 __help__ = """
 
-** Ξ L I N Λ 👸 - A Powerful Telegram Group Manager 🎶 **
+* Ξ L I N Λ 👸 - Anime Themed Powerful Telegram Group Manager Bot.*
 
 ✮───────────────✮
 Powerful Abilities
 ✮───────────────✮
 • File To Link and URL Upload 📂
 • Youtube Downloader 🎵
-Developed by:- [TEAM ELINA](https://t.me/ElinaNews)
+• Serach Books 📚
+• Search Exam Results 🏆
+
+♟ Developed by :- [TEAM ELINA](https://t.me/ElinaNews)
+♟ Source Code :-[Click Here](https://github.com/youtubeslgeekshow/sz-rose-bot)
+
+ ©️@TheElinaBot🇱🇰
 """
 __mod_name__ = "ABOUT🔆"
