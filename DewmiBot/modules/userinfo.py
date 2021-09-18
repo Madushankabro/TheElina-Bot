a011a969fd02b60ecc02c14d821b0606cd591c74
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-18 13:22:38 +0530

    Update userinfo.py

--- DewmiBot/modules/userinfo.py
+++ DewmiBot/modules/userinfo.py
@@ -582,7 +582,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "Info📑"
+__mod_name__ = "INFO📑"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
