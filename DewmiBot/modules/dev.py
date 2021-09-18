54e1e4a6c7348a69e3960d611d169de63eadf667
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-18 08:28:33 +0530

    Update dev.py

--- DewmiBot/modules/dev.py
+++ DewmiBot/modules/dev.py
@@ -67,5 +67,5 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-__mod_name__ = "Dev🙂"
+__mod_name__ = "DEV🙂"
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER]
