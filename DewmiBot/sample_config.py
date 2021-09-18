74ec3140a3610cc08f2458f9e4364d101b58c673
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-18 08:26:22 +0530

    Elina

--- DewmiBot/sample_config.py
+++ DewmiBot/sample_config.py
@@ -18,7 +18,7 @@ class Config(object):
     API_HASH = "API_HASH"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 123456  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "hirunaoficial"
+    OWNER_USERNAME = "Your User Name"
     SUPPORT_CHAT = "sltechzoneofficial"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
